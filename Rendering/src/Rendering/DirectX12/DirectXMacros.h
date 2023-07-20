997944c664b4947c89519fb3077c8bd43dfed06c
Esau Abraham <esau.seattle@gmail.com>
2023-07-19 21:16:58 -0700

    Removed Rendering Folder

--- Rendering/src/Rendering/DirectX12/DirectXMacros.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "../RenderExceptions.h"
-
-#define RENDER_THROW(hrcall) if(FAILED((hrcall))) throw RenderExceptions::HrException(__LINE__,__FILE__, (hrcall) )
-
--- Rendering/src/Rendering/RendererOptions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "DirectX12/DirectXRenderer.h"
-#include "Renderer.h"
-
-enum class RendererOptions {
-	DirectX12
-};
