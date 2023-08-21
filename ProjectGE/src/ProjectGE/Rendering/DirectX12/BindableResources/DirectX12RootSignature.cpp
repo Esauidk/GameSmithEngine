8d36a941ee62d5b2c6befe264c6e632ae90a58ba
Esau Abraham <esau.seattle@gmail.com>
2023-08-21 00:07:28 -0700

    Added documentation for method of analysis for root singature

--- ProjectGE/src/ProjectGE/Rendering/DirectX12/BindableResources/DirectX12RootSignature.cpp
+++ ProjectGE/src/ProjectGE/Rendering/DirectX12/BindableResources/DirectX12RootSignature.cpp
@@ -29,6 +29,40 @@ namespace ProjectGE {
 		Init(builder.Build());
 	}
 
+	void DirectX12RootSignature::IndexRootSignature(D3D12_VERSIONED_ROOT_SIGNATURE_DESC& desc)
+	{
+		// Analysis method taken from Unreal Engine:
+		// Expected Layout of Root Signature is as follows:
+		// EACH STAGE WILL HAVE POTENTIALLY 4 TYPES OF PARAMETERS:
+		// SRV TABLES, CBV TABLES, ROOT CONSTANTS, AND SAMPLERS
+		// 
+		// THERE IS ALSO THE CHANCE OF PARAMETERS THAT BELONG TO ALL STAGES
+		// GLOBAL SRV TABLE, GLOBAL CBV TABLE, GLOBAL ROOT CONSTANTS, GLOBAL SAMPLERS, AND UAVS
+		// 
+		// The goal is to have a mapping of register entries for each type of parameter for all kinds of visibility. 
+		// Anyone can then, prompt for the index before attempting to bind in that slot
+		// 
+		// Another expectation is that the root signature parameter register index should be in the order of the parameters in the description
+		// 
+		// Steps for analysis:
+		// 1) Determine The total size of the root signature (with each parameter offset)
+		// 2) Determine the accessibility of the root signature (which stages can access)
+		// 3) Determine the size and binding slots of the resources
+
+		/*for (int i = 0; i < desc.Desc_1_1.NumParameters; i++) {
+
+			auto& curParam = desc.Desc_1_1.pParameters[i];
+			switch (curParam.ParameterType) {
+			case D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE:
+
+			case D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS:
+			case D3D12_ROOT_PARAMETER_TYPE_CBV:
+			case D3D12_ROOT_PARAMETER_TYPE_SRV:
+			case D3D12_ROOT_PARAMETER_TYPE_UAV:
+			}
+		}*/
+	}
+
 	/*void DirectX12RootSignature::Bind() {
 		auto& cmdList = DirectX12Context::GetDirectCommandList();
 		cmdList->SetGraphicsRootSignature(m_Root.Get());
--- ProjectGE/src/ProjectGE/Rendering/DirectX12/BindableResources/DirectX12RootSignature.h
+++ ProjectGE/src/ProjectGE/Rendering/DirectX12/BindableResources/DirectX12RootSignature.h
@@ -16,17 +16,19 @@ namespace ProjectGE {
 	class DirectX12RootSignature
 	{
 	public: 
-			DirectX12RootSignature() = default;
-			void Init(const D3D12_VERSIONED_ROOT_SIGNATURE_DESC& desc);
-			void InitGenericRootSignature(D3D12_ROOT_SIGNATURE_FLAGS flags);
-			inline ID3D12RootSignature* GetInternalRootSignature() { return m_Root.Get(); }
+		DirectX12RootSignature() = default;
+		void Init(const D3D12_VERSIONED_ROOT_SIGNATURE_DESC& desc);
+		void InitGenericRootSignature(D3D12_ROOT_SIGNATURE_FLAGS flags);
+		inline ID3D12RootSignature* GetInternalRootSignature() { return m_Root.Get(); }
 			//void Bind() override;
 	private:
+		void IndexRootSignature(D3D12_VERSIONED_ROOT_SIGNATURE_DESC& desc);
 		D3D12_FEATURE_DATA_ROOT_SIGNATURE m_RootSigFeat;
 		ComPtr<ID3D12RootSignature> m_Root;
 		UINT m_AvailableSlot;
 		Microsoft::WRL::ComPtr<ID3DBlob> m_RootSigBlob;
 		Microsoft::WRL::ComPtr<ID3DBlob> m_ErrorBlob;
+		UINT RegisterSlotTable[];
 	};
 }
 
